f90216d135ad7f14de97c2ed22338f66cc070ec7
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-01 15:43:57 +0200

    Added Lambda Expressions

---
+++ Advanced_Concepts/Lamda Expressions/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Advanced_Concepts.LamdaExpressions
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+
+
+
+    }
+}
--- Advanced_Concepts/Program.cs
+++ Advanced_Concepts/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Advanced_Concepts.Delegates;
+using Advanced_Concepts.LamdaExpressions;
 
 namespace Advanced_Concepts
 {
@@ -10,6 +11,7 @@ namespace Advanced_Concepts
         {
             #region Generics
 
+            Console.WriteLine("------- Starting Generics ---------");
             var number = new Nullable<int>(0);
             Console.WriteLine("Has Value: "+ number.HasValue);
             Console.WriteLine("Value: " + number.GetValueOrDefault());
@@ -17,6 +19,8 @@ namespace Advanced_Concepts
             #endregion
 
             #region Delegates
+
+            Console.WriteLine("------- Starting Delegates ---------");
             // Use for extesibility and flexibility
             var processor = new PhotoProcessor();
             var photoFilters = new PhotoFilters();
@@ -26,6 +30,21 @@ namespace Advanced_Concepts
             filterHandler += RemoveRedEye;
 
             processor.Process("Photos.jpg", filterHandler);
+            #endregion
+
+            #region Lambda Expressions
+
+            Console.WriteLine("------- Starting Lambda Expressions ---------");
+            const int factor = 5;
+            Func<int, int> multiplier = num => num * factor;
+            Console.WriteLine("The result after multiplication: " + multiplier(50));
+
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books.FindAll(b => b.Price <= 35);
+            Console.WriteLine("Books having the price less than 35: ");
+            foreach (var book in cheapBooks)
+                Console.WriteLine(book.Title);
+
             #endregion
         }
 
